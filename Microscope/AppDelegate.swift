5f760c913798410b07f557b2d5215cd4022fe443
drhaynes <drhmbp@gmail.com>
2019-04-27 15:06:32 +0100

    Initial Commit

---
+++ Microscope/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Microscope
+//
+//  Created by David Haynes on 27/04/2019.
+//  Copyright © 2019 MBP Consulting Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Microscope/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Microscope
+//
+//  Created by David Haynes on 27/04/2019.
+//  Copyright © 2019 MBP Consulting Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
