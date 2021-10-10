9a235fa1bf8f8da4dcec666ca8fcd11cc395c14c
drhaynes <david@mbpapps.com>
2021-10-10 13:28:33 +0100

    Use the ViewController rather than a panel; update camera privacy string in plist

--- Microscope/ViewController.swift
+++ Microscope/ViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var captureSession: AVCaptureSession!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // TODO: Calculate this based on the monitor resolution
+        let width = CGFloat(640)
+        let height = CGFloat(480)
+        
+        //TODO: Use this to position the window initiall in the centre of the screen
+        let screenRect = NSScreen.main!.frame
+        let xPos = (screenRect.width / 2.0) - (width / 2.0)
+        let yPos = (screenRect.height / 2.0) - (height / 2.0)
+
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.medium
+        let webcamCaptureDevice = AVCaptureDevice.devices(for: .video)[0]
+        let webcamInput = (try! AVCaptureDeviceInput(device: webcamCaptureDevice))
+        captureSession.addInput(webcamInput)
+
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        videoPreviewLayer.frame = CGRect(x: -width, y: 0, width: width, height: height)
+
+        let viewLayer = CALayer()
+        viewLayer.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        viewLayer.addSublayer(videoPreviewLayer)
+        viewLayer.sublayerTransform = CATransform3DMakeScale(-1, 1, 1)
+
+        view.wantsLayer = true
+        view.layer = viewLayer
+
+        captureSession.startRunning()
     }
 
     override var representedObject: Any? {
@@ -22,6 +52,7 @@ class ViewController: NSViewController {
         }
     }
 
-
+    override func viewDidDisappear() {
+        captureSession.stopRunning()
+    }
 }
-
