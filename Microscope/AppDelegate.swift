8caf97599d3b070ad0c8ee09b67c6807406ecb0c
drhaynes <david@mbpapps.com>
2021-10-10 14:35:36 +0100

    Use a layer-backed view for capture, rather than a sublayer

--- Microscope/AppDelegate.swift
+++ Microscope/AppDelegate.swift
@@ -12,7 +12,5 @@ import AVFoundation
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) { }
-
     func applicationWillTerminate(_ aNotification: Notification) { }
 }
-
--- Microscope/ViewController.swift
+++ Microscope/ViewController.swift
@@ -15,41 +15,16 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // TODO: Calculate this based on the monitor resolution
-        let width = CGFloat(640)
-        let height = CGFloat(480)
-        
-        //TODO: Use this to position the window initiall in the centre of the screen
-        let screenRect = NSScreen.main!.frame
-        let xPos = (screenRect.width / 2.0) - (width / 2.0)
-        let yPos = (screenRect.height / 2.0) - (height / 2.0)
-
         captureSession = AVCaptureSession()
-        captureSession.sessionPreset = AVCaptureSession.Preset.medium
-        let webcamCaptureDevice = AVCaptureDevice.devices(for: .video)[0]
-        let webcamInput = (try! AVCaptureDeviceInput(device: webcamCaptureDevice))
-        captureSession.addInput(webcamInput)
-
+        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        let lastVideoCaptureDevice = AVCaptureDevice.devices(for: .video).last!
+        let videoCaptureInput = (try! AVCaptureDeviceInput(device: lastVideoCaptureDevice))
+        captureSession.addInput(videoCaptureInput)
         videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        videoPreviewLayer.frame = CGRect(x: -width, y: 0, width: width, height: height)
-
-        let viewLayer = CALayer()
-        viewLayer.frame = CGRect(x: 0, y: 0, width: width, height: height)
-        viewLayer.addSublayer(videoPreviewLayer)
-        viewLayer.sublayerTransform = CATransform3DMakeScale(-1, 1, 1)
-
         view.wantsLayer = true
-        view.layer = viewLayer
-
+        view.layer = videoPreviewLayer
+        view.layer?.backgroundColor = .black
         captureSession.startRunning()
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
     override func viewDidDisappear() {
